2d340e1799a04ef58002b1f1adf650fb040f0178
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-26 04:00:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,13 +18,12 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      puts "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
+    if @status == "complete"
       puts "Transaction was already excuted"
-
+    else
+      @sender.deposit( @amount * -1 )
+      @receiver.deposit( @amount )
+      @status = "complete"
     end
   end
 
