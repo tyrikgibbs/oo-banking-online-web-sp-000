a7ae12522344b6f72af70147d30ed8fe160c4ca3
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-26 04:04:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,13 +18,13 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-  @sender.balance -= @amount
-  @receiver.balance += @amount
-  @status = "complete"
+    if valid? && sender.balance > amount && self.status == "pending"
+    sender.withdrawal(self.amount)
+    receiver.deposit(self.amount)
+    self.status = "complete"
   else
-  @status = "rejected"
-  return "Transaction rejected. Please check your account balance."
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
     end
   end
 
